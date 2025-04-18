b00a39bdba9b85b9be6da64bfd11bb0fab0a746f
aidan-96 <140011470@dundee.ac.uk>
2025-04-18 10:51:28 +0100

    wq

--- mergestrings.py
+++ mergestrings.py
@@ -41,7 +41,7 @@ def mergeAlternately(word1, word2):
         result = []
         n = max(len(word1), len(word2))
         for i in range(n):
-            if i < len(word1):
+            if i < len(word1): 
                 result += word1[i]
             if i < len(word2):
                 result += word2[i]
